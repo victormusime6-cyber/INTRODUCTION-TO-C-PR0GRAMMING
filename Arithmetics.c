cd97effc23cd449b25abae2ac32c4212d3c6b887
victormusime6-cyber <victormusime6@gmail.com>
2026-05-16 13:54:35 +0300

    variables working with airthmetic symbols.

---
+++ Arithmetics.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    int a = 2;
+    int b = 5;
+    //int x = a+b;
+
+    //int x = b-a;
+    //int x = a*b;
+
+    //float x = b/a;
+
+    //int x = b%a; 
+    //int x = a%2;
+
+    //b++;
+
+    //AUGMENTED ASSIGNED OPERATORS
+
+    //b+=3;
+
+    //THE LONGER VERSION LOOKS SOMETHING LIKE THIS
+    //b = b+3
+    //b*=2;
+    printf("%d", b);
+    return 0;
+}
