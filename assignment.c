b9f7053efae3944d619bfa72dd4d4544f883a74a
victormusime6-cyber <victormusime6@gmail.com>
2026-05-16 14:50:13 +0300

    Update of functional precision.

---
+++ assignment.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+ int main(){
+    
+    bool isOnline = false;
+    /*The specifier for booleans s%d.
+    This is to check if the user is online.*/
+    printf("%d", isOnline);
+
+    return 0;
+ }
