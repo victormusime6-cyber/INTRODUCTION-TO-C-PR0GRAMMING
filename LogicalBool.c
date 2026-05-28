6cae806f49ef64fe573dba4ce4ffb3c96ac09775
victormusime6-cyber <victormusime6@gmail.com>
2026-05-28 20:27:52 +0300

    If statements with Booleans

---
+++ LogicalBool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main (){
+     
+    bool isSunny = true;
+
+    if(!isSunny){
+        printf("It's CLOUDY outside.");
+    }
+    else{
+        printf("It's SUNNY outside.");
+    }
+    return 0;
+}
