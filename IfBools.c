00a6c761f6eb3ad200d6bfc7ce135abbf95b6974
victormusime6-cyber <victormusime6@gmail.com>
2026-05-28 20:19:29 +0300

    Boleans with If statement

---
+++ IfBools.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+
+    bool isAdult = false;
+
+    if( isAdult == true){
+        printf("You are an adult.");
+    }
+    else{
+        printf("You are a child.");
+    }
+    return 0;
+}
