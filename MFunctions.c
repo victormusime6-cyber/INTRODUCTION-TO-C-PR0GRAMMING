b32275974abaf8c360a8dd6b3c1d1cb5507051e9
victormusime6-cyber <victormusime6@gmail.com>
2026-05-28 19:53:47 +0300

    Arithmetic C Functions

---
+++ MFunctions.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    //MATH FUNCTIONS
+
+    float x = 45;
+    //x = sqrt(x);
+    //x = pow(x,3);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x);
+    //x = log(x);
+    //x = sin(x); 
+    //x = cos(x);
+    //x = tan(x);
+
+    printf("%f", x);
+
+    return 0;
+}
