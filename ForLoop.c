1871d8d14cd1f7562ad9d85bd00748815a7a28a4
victormusime6-cyber <victormusime6@gmail.com>
2026-05-30 17:36:46 +0300

    A for loop

---
+++ ForLoop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    /* The for loop can repeat code a specified number of times.*/
+    // for(initialization;Condition;Update)
+    for(int x = 0; x < 20; x++){
+    printf("%i\n",x);
+}
+
+    return 0;
+}
